c54ba82c21dddafad98f0a8c497b43d144e8aa3f
Doraemon1 <m.ilham2000r@gmail.com>
2024-12-16 04:37:37 +0700

    edit port

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const taskRoutes = require('./routes/taskRoutes');
 dotenv.config()
 
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = 3000
 
 // Middleware
 app.use(express.json())
